9f7e64006ff964356bc15bfd631d193ff4a387b6
raelix <raelix@desktop-debian.home>
2018-02-24 00:20:00 +0100

    fixed project permission - revert

--- init-script.sh
+++ init-script.sh
@@ -9,10 +9,4 @@ cp system-config/sshd_config /etc/ssh/
 cp system-config/.pythonrc ~/.pythonrc
 cp system-config/.bashrc ~/.bashrc
 npm install
-npm run build
-pushd server/
-npm install
-npm run server &
-sleep 2
-popd
 npm start
